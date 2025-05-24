477cd90fad138004254b3ca3f7140d720f4eb5a1
Nikolay Yurin <github@yurinnick.dev>
2025-05-23 23:21:52 -0700

    Update actions

--- run_once_setup_tmux.sh
+++ run_once_setup_tmux.sh
@@ -5,5 +5,5 @@ sudo dnf install tmux
 TPM_DIR="$HOME/.tmux/plugins/tpm"
 if [ ! -d "$TPM_DIR" ];then
     mkdir -p "$TPM_DIR"
-    git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+    git clone --depth=1 https://github.com/tmux-plugins/tpm "$TPM_DIR"
 fi
--- run_once_setup_vim.sh
+++ run_once_setup_vim.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
 sudo dnf install -y vim neovim
-
-NVIM_PLUGINS_DIR="$HOME/.local/share/nvim/site/autoload"
-VIM_PLUGINS_DIR="$HOME/.vim/autoload"
-
-mkdir -p "$NVIM_PLUGINS_DIR"
-mkdir -p "$VIM_PLUGINS_DIR"
-
-if [ ! -f "$NVIM_PLUGINS_DIR/plug.vim" ]; then
-  curl -fLo "$NVIM_PLUGINS_DIR/plug.vim" --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-  ln -s "$NVIM_PLUGINS_DIR/plug.vim" "$VIM_PLUGINS_DIR/plug.vim"
-fi
